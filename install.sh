d93cf88122ccbfe7f46753eb12262c72a02a755e
Ruan Aragão <ruanaragao2@gmail.com>
2016-07-12 14:16:31 -0300

    Add encoding

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 echo "Init install"
+Encoding=UTF-8
 mv "/home/${USER}/.gimp-2.8" "/home/${USER}/.gimp-2.8.old"
-curl -L "https://github.com/RuanAragao/photogimp/blob/packet/PhotoGIMP-gimp-2.8.tar.bz2" -o "/home/${USER}/photogimp-g28.tar.bz2"
+curl -L "https://github.com/RuanAragao/photogimp/blob/packet/PhotoGIMP-gimp-2.8.tar.bz2" -o "/home/${USER}/PhotoGIMP-gimp-2.8.tar.bz2"
 cd /home/${USER}
-tar -xvjf "photogimp-g28.tar.bz2"
+tar -xvjf "PhotoGIMP-gimp-2.8.tar.bz2"
 echo "Finish!"
