728afddc34580125c4761789682d8e48d83a4e24
Ruan Aragão <ruanaragao2@gmail.com>
2016-07-12 14:23:12 -0300

    Remove file after installationwq

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 echo "Init install"
 Encoding=UTF-8
 mv "/home/${USER}/.gimp-2.8" "/home/${USER}/.gimp-2.8.old"
-curl -L "https://github.com/RuanAragao/photogimp/blob/packet/PhotoGIMP-gimp-2.8.tar.bz2" -o "/home/${USER}/PhotoGIMP-gimp-2.8.tar.bz2"
+curl -L "https://github.com/RuanAragao/photogimp/blob/packet/PhotoGIMP-gimp-2.8.tar.bz2" -o "/home/${USER}/PhotoGIMP-gimp-2.8.tar.bz2?raw=true"
 cd /home/${USER}
 tar -xvjf "PhotoGIMP-gimp-2.8.tar.bz2"
+rm "PhotoGIMP-gimp-2.8.tar.bz2"
 echo "Finish!"
