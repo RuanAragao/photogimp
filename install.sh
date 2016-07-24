b72051d4b416710901b40ad4f9d755a3b33e09df
Ruan Aragão <ruanaragao2@gmail.com>
2016-07-24 13:18:56 -0300

    Fix install file

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 echo "Init install"
 Encoding=UTF-8
-mv "/home/${USER}/.gimp-2.8" "/home/${USER}/.gimp-2.8.old"
-curl -L "https://github.com/RuanAragao/photogimp/blob/packet/PhotoGIMP-gimp-2.8.tar.bz2" -o "/home/${USER}/PhotoGIMP-gimp-2.8.tar.bz2?raw=true"
+gimp_dir="/home/${USER}/.gimp-2.8"
+if [ -d "${gimp_dir}" ]; then
+	echo "Creating GIMP data backup"
+	mv "${gimp_dir}" "/home/${USER}/.gimp-2.8.old"
+fi
+echo "Dowloading PhotoGIMP"
+curl -L "https://github.com/RuanAragao/photogimp/blob/packet/PhotoGIMP-gimp-2.8.tar.bz2?raw=true" -o "/home/${USER}/PhotoGIMP-gimp-2.8.tar.bz2"
 cd /home/${USER}
+echo "Installing PhotoGIMP"
 tar -xvjf "PhotoGIMP-gimp-2.8.tar.bz2"
 rm "PhotoGIMP-gimp-2.8.tar.bz2"
 echo "Finish!"
